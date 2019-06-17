c7059642555c6b97bc516f39285e5b0f1e128147
Neto <neeto_alves@hotmail.com>
2019-06-17 06:44:53 -0300

    Sobre escreve metodo as_json

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,15 +2,22 @@ class Contact < ApplicationRecord
   belongs_to :kind
   has_many :phones
 
-  def birthdate_br
-    I18n.l(self.birthdate) unless self.birthdate.blank?
+  def as_json(options={})
+    h = super(options)
+    h[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    h
   end
 
-  def to_br
-    { name: self.name,
-      email: self.email,
-      birthdate:(I18n.l(self.birthdate) unless self.birthdate.blank?) }
-  end
+
+  # def birthdate_br
+  #   I18n.l(self.birthdate) unless self.birthdate.blank?
+  # end
+  #
+  # def to_br
+  #   { name: self.name,
+  #     email: self.email,
+  #     birthdate:(I18n.l(self.birthdate) unless self.birthdate.blank?) }
+  # end
 
 
   # def author
